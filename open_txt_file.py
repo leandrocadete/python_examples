829080a941bdba8889fa3d744e5e9afe0ddadd6a
Leandro Cadete da Silva <leandro.cadete.cadete@hotmail.com>
2021-05-04 19:32:45 -0300

    Read and write to file

--- open_txt_file.py
+++ open_txt_file.py
@@ -1,5 +1,11 @@
-f = open("myfile.txt", "a")
+import os
+
+f = open("myfile.txt", "w")
 for i in range(10):    
-    f.write("line -> \n")
-   # print("$i")
+    f.write("line -> " + str(i) + "\n")   
 f.close()
+
+fo = open("myfile.txt", "r")
+for s in  fo.readlines():
+    print(s)
+fo.close()
